7c96387125092009e7e1259cad3f5f2508a15b85
Dida <dimitrije.despotovski@yahoo.com>
2025-08-05 19:15:16 +0200

    Update client.lua
    Added /salesman to spawn a ped and made him interactable

--- client/client.lua
+++ client/client.lua
@@ -1,17 +1,44 @@
+created = nil
+interactDistance = 3
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local playerCoords = GetEntityCoords(PlayerPedId())
+        local pedCoords = GetEntityCoords(created)
+        local dist = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, pedCoords.x, pedCoords.y, pedCoords.z, true)
+
+        if dist < interactDistance then
+            --[TODO: display press E to interact]
+
+            if IsControlJustPressed(0, 38) then -- 38 is E key
+                print('Works') --[TODO: display UI]
+            end
+        end
+    end
+end)
+RegisterCommand('salesman', function()
+    local player = PlayerPedId()
+    local ped = 'a_m_m_hillbilly_01'
+    RequestModel(ped)
+    repeat Wait(0) until HasModelLoaded(ped)
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    
+    created = CreatePed(CIVMALE, ped, playerCoords.x, playerCoords.y + 1, playerCoords.z, 180, true, true)
+    Wait(500)
+    FreezeEntityPosition(created,true)
+    SetPedConfigFlag(created, 0, true)
+    SetEntityInvincible(created, true)
+    
+
+
+end)
+
 RegisterCommand('svelte:show', function()
     SendNUI('setVisible', true)
     SetNuiFocus(true, true)
     print("Svelte showing")
 end, false)
 
-RegisterNUICallback('getClientData', function(_, cb)
-    local playerCoords = GetEntityCoords(PlayerPedId())
-    cb({
-        x = math.ceil(playerCoords.x),
-        y = math.ceil(playerCoords.y),
-        z = math.ceil(playerCoords.z)
-    })
-end)
 
 RegisterNUICallback('hideUI', function(_, cb)
     cb({})
